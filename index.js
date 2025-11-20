e51e06e6535903e1b774d30c09dfda36ae714044
Anurag Shre <aquaflames2005@gmail.com>
2025-11-20 18:39:48 +0530

    done

--- index.js
+++ index.js
@@ -2,12 +2,42 @@
 function check() {
   let num = Number(prompt("Enter a Number: "));
   while (true) {
-    if(Number.isNaN(num)){
-        num = Number(prompt("Enter a \"Valid\" Number: "));
-        continue
-    }
-    else{
-        return num
+    if (Number.isNaN(num)) {
+      num = Number(prompt('Enter a "Valid" Number: '));
+      continue;
+    } else {
+      return num;
     }
   }
+}
+
+const a = check();
+
+const arr = ["+", "-", "/", "*"];
+let s = prompt("Enter an operation: '+' or '-' or '*' or '/' ");
+while (!arr.includes(s)) {
+  s = prompt(
+    "Enter a 'Valid Opteration' these are the only choice: '+' or '-' or '*' or '/' "
+  );
+}
+
+const b = check();
+
+switch (s) {
+  case "+":
+    alert(a + b);
+    console.log(a + b);
+    break;
+  case "-":
+    alert(a - b);
+    console.log(a - b);
+    break;
+  case "/":
+    alert(a / b);
+    console.log(a / b);
+    break;
+  case "*":
+    alert(a * b);
+    console.log(a * b);
+    break;
 }
